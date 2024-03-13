102fa0447c98ad2eb3d4effd05b6f6a38913fe1b
Thada Wangthammang <thada.wth@gmail.com>
2024-03-14 02:26:28 +0700

    add morgan

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,10 +1,13 @@
 import express from 'express';
+import morgan from 'morgan';
 import routes from './root-routes';
 
   const host = process.env.HOST ?? 'localhost';
   const port = process.env.PORT ? Number(process.env.PORT) : 3000;
 
   const app = express();
+
+  app.use(morgan('dev'));
 
   app.use('/', routes);
 
